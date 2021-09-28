c92f2983b5bfcab4de72063d417b94bebeb65e98
Mark A. Tsuchida <matsuchida@wisc.edu>
2021-09-28 12:27:58 -0500

    Update fix for BH SDT file header
    Now works with BH's "TCSPC Package 6.6.0".

--- fix-spc-data-file-structure-h.sh
+++ fix-spc-data-file-structure-h.sh
@@ -13,6 +13,8 @@ mkdir -p generated_include
 #   MOMParam (uses undefined type Window)
 #   WndLayout (uses undefined type BHPanelData)
 #   GR_Param32 (uses Trace2d32 before it's defined)
+#   HST3Dtrace (uses undefined type Interval)
+#   HST3DParam (uses HST3Dtrace)
 # Rename first of the two definitions of BHFileBlockHeader (append 'Old')
 # Replace U_LONG, U_INT, U_SHORT, U_CHAR with corresponding types
 # Make char[32] difinitions 'static' to prevent link errors
@@ -21,6 +23,8 @@ tac '/c/Program Files (x86)/BH/SPCM/SPC_data_file_structure.h' \
 	|sed '/}MOMParam;/,/typedef struct/d' \
 	|sed '/}WndLayout;/,/typedef struct/d' \
 	|sed '/}GR_Param32;/,/typedef struct/d' \
+	|sed '/}HST3Dtrace;/,/typedef struct/d' \
+	|sed '/}HST3DParam;/,/typedef struct/d' \
 	|tac \
 	|sed '0,/}BHFileBlockHeader;/ s/Header/HeaderOld/' \
 	|sed 's/U_LONG/unsigned long/' \
